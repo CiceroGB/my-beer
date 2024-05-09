671a8f0fcf0b760369581f9e259d1b59989ba194
Cicero Gasparini Brunelli <63745509+CiceroGB@users.noreply.github.com>
2024-05-09 01:36:30 +0000

    add model BeerIngredient

---
+++ app/MyBeer.Infrastructure/Configurations/IngredientConfiguration.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBeer.Domain.Entities;
+
+namespace MyBeer.Infrastructure.Configurations;
+
+internal class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
+{
+    public void Configure(EntityTypeBuilder<Ingredient> builder)
+    {
+        builder.HasKey(ingredient => ingredient.Id);
+        builder.Property(ingredient => ingredient.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+    }
+}
