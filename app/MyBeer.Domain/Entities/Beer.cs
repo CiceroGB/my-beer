d015fa090edb49659f2e9dc65f43ddf345beb17f
Cicero Gasparini Brunelli <63745509+CiceroGB@users.noreply.github.com>
2024-05-09 03:19:10 +0000

    fix class

--- app/MyBeer.Domain/Entities/Beer.cs
+++ app/MyBeer.Domain/Entities/Beer.cs
@@ -1,24 +1,28 @@
+using System;
+using System.Collections.Generic;
+
 namespace MyBeer.Domain.Entities
 {
     public class Beer
     {
         public Guid Id { get; private set; } = Guid.NewGuid();
         public string Name { get; private set; }
-        private List<Guid> _ingredients;
-        public IReadOnlyList<Guid> Ingredients => _ingredients.AsReadOnly();
+        private List<Guid> _ingredients = new List<Guid>(); 
 
+        public IReadOnlyList<Guid> Ingredients => _ingredients.AsReadOnly();
 
         public Beer(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
-            Name = name;
+            Name = name; 
         }
-
 
         public void AddIngredient(Guid ingredientId)
         {
-            _ingredients.Add(ingredientId);
+            if (ingredientId == Guid.Empty)
+                throw new ArgumentException("Ingredient ID cannot be empty.", nameof(ingredientId));
+            _ingredients.Add(ingredientId); 
         }
     }
 }
--- app/MyBeer.Domain/Entities/Ingredient.cs
+++ app/MyBeer.Domain/Entities/Ingredient.cs
@@ -1,20 +1,28 @@
+
 namespace MyBeer.Domain.Entities
 {
     public class Ingredient
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Name { get; private set; }
-        public string Description { get; set; }
+        public Guid Id { get; private set; } = Guid.NewGuid(); 
+        public string Name { get; private set; } 
+        public string Description { get; private set; } 
 
         public Ingredient(string name, string description)
         {
             if (string.IsNullOrWhiteSpace(name))
-                throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+                throw new ArgumentException("Name cannot be null or whitespace..", nameof(name));
             if (string.IsNullOrWhiteSpace(description))
                 throw new ArgumentException("Description cannot be null or whitespace.", nameof(description));
 
             Name = name;
             Description = description;
+        }
+
+        public void UpdateDescription(string newDescription)
+        {
+            if (string.IsNullOrWhiteSpace(newDescription))
+                throw new ArgumentException("Description", nameof(newDescription));
+            Description = newDescription;
         }
     }
 }
